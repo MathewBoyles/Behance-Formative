c321c26c695fe6718e7b37e3902133b6e31f6fa1
mathew boyles <hello@mathewboyles.com>
2017-09-19 09:17:50 +1200

    Popup functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,43 @@
+$.fn.popup = function(fun, options) {
+  if(fun == "toggle") {
+    this.popup((this.is(":visible") ? "hide" : "show"), options);
+    return this;
+  }
+
+  this.find(".popup-backdrop")
+    .filter(function() {
+      return !$(this).data("popupInit");
+    })
+    .data({"popupInit": 1})
+    .click(function() {
+      $(this).parent().popup("hide");
+    });
+
+  if(fun == "hide") {
+    this.find(".popup-container")
+      .animate({
+        "top": 0 - (this.find(".popup-container").height()*1.5)
+      });
+    this.find(".popup-backdrop")
+      .fadeOut(500, function() {
+        $(this).parent().hide();
+      });
+  }
+  else if (fun == "show" || !fun) {
+    this.find(".popup-backdrop,.popup-container").hide();
+    this.show();
+    this.find(".popup-container")
+      .css({"top": 0 - (this.find(".popup-container").height()*1.5)})
+      .show()
+      .animate({
+        "top": "50%"
+      });
+    this.find(".popup-backdrop").fadeIn(500);
+  }
+
+  return this;
+};
 
 
 
-// Template ajax request
-// $.ajax({
-//   url: "url",
-//   dataType: "json",
-//   success: function(data){
-//     console.log(data);
-//   },
-//   error: function(){
-//     console.log("something went wrong.");
-//   }
-// });
+$("#test-popup").popup("show");

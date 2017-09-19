44aa78e4ba2aa26d923c8a0734cc57d40d3ef71e
Mathew Boyles <hello@mathewboyles.com>
2017-09-20 09:13:31 +1200

    Removed page extension from links

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-var express = require('express');
-var path = require('path');
-
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
 var app = express();
 
-app.use(function(request, reponse, next){
+app.use(function(request, reponse, next) {
   console.log(`${request.method} request for ${request.url}`);
+  next();
+});
+
+app.use(function(request, reponse, next) {
+  var pageName = request.url.split("?")[0];
+  if(pageName == "/home") pageName = "/index";
+
+  var fullLink = path.join(__dirname, "public", pageName + ".html");
+  if(fs.existsSync(fullLink)) {
+    reponse.header({"Content-Type": "text/html"});
+    reponse.send(fs.readFileSync(fullLink));
+  }
+
   next();
 });
 
 app.use(express.static("./public"));
 
-app.use('/packages', express.static(path.join(__dirname, 'node_modules/')));
+app.use("/packages", express.static(path.join(__dirname, "node_modules/")));
 
 app.listen(3000);
 

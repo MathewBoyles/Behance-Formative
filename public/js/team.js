1b757c7126626e8eb3fb74757e8b78d0ce33b737
Mathew Boyles <hello@mathewboyles.com>
2017-09-21 11:51:27 +1200

    Merged omri and master

--- public/js/team.js
+++ public/js/team.js
@@ -1,3 +1,5 @@
+var teamReady = false;
+
 if ($("body").attr("id") == "about") {
   $.getJSON("/js/team.json", function(team) {
     var template = $("#teamRows").html();
@@ -6,16 +8,17 @@ if ($("body").attr("id") == "about") {
     var teamItems = [];
     for (var i = 0; i < team.length; i++) {
       var teamItem = team[i];
-      teamItem.lineBr = ((i+1) % 4) == 0;
+      teamItem.lineBr = ((i + 1) % 4) == 0;
       teamItems.push(teamItem);
     }
 
     var context = {
-        team: teamItems
+      team: teamItems
     };
     var html = compiledTemplate(context);
     $("#teamRows").after(html);
 
-    $("#loading").fadeOut();
+    teamReady = true;
+    if (mapReady) $("#loading").fadeOut();
   });
 }

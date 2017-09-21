b3aad6e3d55d3ab0f4a6b5db0f5587f2bb25b6ad
Mathew Boyles <hello@mathewboyles.com>
2017-09-21 18:06:33 +1200

    HTTPS

--- public/js/main.js
+++ public/js/main.js
@@ -460,7 +460,7 @@ if ($("body").attr("id") == "profile") {
   portfolio.user = profileID;
 
   $.ajax({
-    url: "http://www.behance.net/v2/users/" + profileID,
+    url: "https://www.behance.net/v2/users/" + profileID,
     data: {
       client_id: config.client_id
     },

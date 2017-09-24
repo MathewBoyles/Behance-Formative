664a2af0e2568e99db99c392a0cf7cb373c63432
Mathew Boyles <hello@mathewboyles.com>
2017-09-25 11:18:43 +1300

    ScrollTop button added

--- public/js/main.js
+++ public/js/main.js
@@ -686,3 +686,25 @@ if ($("body").attr("id") == "about") {
     if (mapReady) $("#loading").fadeOut();
   });
 }
+
+// Source: top.js
+$(window).scroll(function() {
+  if ($(window).scrollTop() >= 150) $("#toTop:hidden").fadeIn(500);
+  else $("#toTop:not(.fading)").addClass("fading").fadeOut(500, function() {
+    $(this).removeClass("fading");
+  });
+});
+
+$el = $("<div />");
+$el
+  .attr("id", "toTop")
+  .click(function() {
+    $("body").animate({
+      scrollTop: 0
+    }, 500);
+  })
+  .append("<i />")
+  .find("i:last")
+  .addClass("fa")
+  .addClass("fa-chevron-up");
+$("body").append($el);

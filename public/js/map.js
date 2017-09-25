461c1cd2dc33873961d93cfdd2ce88f362f88b1a
Mathew Boyles <hello@mathewboyles.com>
2017-09-26 11:35:37 +1300

    Map lightened

--- public/js/map.js
+++ public/js/map.js
@@ -13,163 +13,171 @@ function initMap() {
       fullscreenControl: false,
       draggable: false,
       styles: [{
-        "featureType": "all",
-        "elementType": "labels",
-        "stylers": [{
+          "featureType": "all",
+          "elementType": "labels",
+          "stylers": [{
+              "color": "#ff0000"
+            },
+            {
+              "visibility": "off"
+            }
+          ]
+        }, {
+          "featureType": "all",
+          "elementType": "labels.text",
+          "stylers": [{
+              "color": "#c4c4c4"
+            },
+            {
+              "visibility": "off"
+            }
+          ]
+        }, {
+          "featureType": "all",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+              "color": "#c4c4c4"
+            },
+            {
+              "visibility": "off"
+            }
+          ]
+        }, {
+          "featureType": "all",
+          "elementType": "labels.icon",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "administrative",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        },
+        {
+          "featureType": "administrative.country",
+          "elementType": "geometry.stroke",
+          "stylers": [{
+            "color": "#c8c1c1"
+          }]
+        }, {
+          "featureType": "administrative",
+          "elementType": "labels",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "administrative",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+            "color": "#c4c4c4"
+          }]
+        }, {
+          "featureType": "administrative.country",
+          "elementType": "labels",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "administrative.province",
+          "elementType": "labels",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [{
+            "color": "#d4d4d4"
+          }]
+        }, {
+          "featureType": "landscape.man_made",
+          "elementType": "all",
+          "stylers": [{
             "color": "#ff0000"
-          },
-          {
-            "visibility": "off"
-          }
-        ]
-      }, {
-        "featureType": "all",
-        "elementType": "labels.text",
-        "stylers": [{
+          }]
+        }, {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [{
+              "visibility": "off"
+            },
+            {
+              "color": "#c4c4c4"
+            }
+          ]
+        }, {
+          "featureType": "poi",
+          "elementType": "geometry.fill",
+          "stylers": [{
             "color": "#c4c4c4"
-          },
-          {
+          }]
+        }, {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [{
+              "saturation": -100
+            },
+            {
+              "lightness": 45
+            },
+            {
+              "color": "#c4c4c4"
+            }
+          ]
+        }, {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [{
+            "visibility": "simplified"
+          }]
+        }, {
+          "featureType": "road.arterial",
+          "elementType": "labels.icon",
+          "stylers": [{
             "visibility": "off"
-          }
-        ]
-      }, {
-        "featureType": "all",
-        "elementType": "labels.text.fill",
-        "stylers": [{
-            "color": "#c4c4c4"
-          },
-          {
-            "visibility": "off"
-          }
-        ]
-      }, {
-        "featureType": "all",
-        "elementType": "labels.icon",
-        "stylers": [{
-          "visibility": "off"
-        }]
-      }, {
-        "featureType": "administrative",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "visibility": "off"
-        }]
-      }, {
-        "featureType": "administrative",
-        "elementType": "labels",
-        "stylers": [{
-          "visibility": "off"
-        }]
-      }, {
-        "featureType": "administrative",
-        "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#c4c4c4"
-        }]
-      }, {
-        "featureType": "administrative.country",
-        "elementType": "labels",
-        "stylers": [{
-          "visibility": "off"
-        }]
-      }, {
-        "featureType": "administrative.province",
-        "elementType": "labels",
-        "stylers": [{
-          "visibility": "off"
-        }]
-      }, {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [{
-          "color": "#c4c4c4"
-        }]
-      }, {
-        "featureType": "landscape.man_made",
-        "elementType": "all",
-        "stylers": [{
-          "color": "#ff0000"
-        }]
-      }, {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [{
-            "visibility": "off"
-          },
-          {
-            "color": "#c4c4c4"
-          }
-        ]
-      }, {
-        "featureType": "poi",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#c4c4c4"
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [{
-            "saturation": -100
-          },
-          {
-            "lightness": 45
-          },
-          {
-            "color": "#c4c4c4"
-          }
-        ]
-      }, {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [{
-          "visibility": "simplified"
-        }]
-      }, {
-        "featureType": "road.arterial",
-        "elementType": "labels.icon",
-        "stylers": [{
-          "visibility": "off"
-        }]
-      }, {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [{
-            "visibility": "off"
-          },
-          {
-            "color": "#c4c4c4"
-          }
-        ]
-      }, {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [{
+          }]
+        }, {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [{
+              "visibility": "off"
+            },
+            {
+              "color": "#c4c4c4"
+            }
+          ]
+        }, {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [{
+              "color": "#ffffff"
+            },
+            {
+              "visibility": "on"
+            }
+          ]
+        }, {
+          "featureType": "water",
+          "elementType": "geometry.fill",
+          "stylers": [{
             "color": "#ffffff"
-          },
-          {
-            "visibility": "on"
-          }
-        ]
-      }, {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#ffffff"
-        }]
-      }, {
-        "featureType": "water",
-        "elementType": "labels",
-        "stylers": [{
-          "visibility": "off"
-        }]
-      }, {
-        "featureType": "water",
-        "elementType": "labels.text",
-        "stylers": [{
-          "color": "#c4c4c4"
-        }]
-      }]
+          }]
+        }, {
+          "featureType": "water",
+          "elementType": "labels",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "water",
+          "elementType": "labels.text",
+          "stylers": [{
+            "color": "#c4c4c4"
+          }]
+        }
+      ]
     };
 
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
